4109d228900df4630f90a7896797525c49f03038
bershakvasya <76277340+bershakvasya@users.noreply.github.com>
2021-07-01 23:42:43 +0300

    FirstMenu

---
+++ menu/Game1.cs
@@ -0,0 +1,213 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using menu.UI;
+using System;
+using System.Collections.Generic;
+
+namespace menu
+{
+    public enum GameState
+    {
+        Menu, Play, Exit, Info, Settings, Extensions, FullScreen, Window, WindowFrame, Sound, GameMenu
+    }
+
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private Menu menu;
+        private Random r;
+        private Background bg;
+        private List<string> mainMenu = new List<string>() {
+            "New game","continue","settings","Developer`s info","Exit"
+        };
+        private List<string> settingsMenu = new List<string> { "extensions", "Sound", "back" };
+        private List<string> infoMenu = new List<string> { "back" };
+        private List<string> soundMenu = new List<string> { "music", "sound", "back" };
+        private List<string> extensionsMenu = new List<string> { "full screen", "window", "window without a frame", "back" };
+        private List<string> gameMenu = new List<string> { "continue", "restart level", "settings", "quit to menu" };
+        public static GameState gameState;
+        public static GameState prevGameState;
+
+        private SpriteFont spriteFont;
+        private string info = "We are game developers and we made this game.";
+
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            menu = new Menu();
+
+            gameState = GameState.Menu;
+            prevGameState = GameState.Menu;
+
+            r = new Random();
+            bg = new Background(_graphics.PreferredBackBufferWidth,
+                _graphics.PreferredBackBufferHeight, "back" + r.Next(0, 10));
+
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            bg.LoadContent(Content);
+
+            menu.LoadContent(Content);
+
+            spriteFont = Content.Load<SpriteFont>("infofont");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            //if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            //    Exit();
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
+                gameState = GameState.GameMenu;
+                prevGameState = GameState.GameMenu;
+            }
+
+            // TODO: Add your update logic here
+            switch (gameState)
+            {
+                case GameState.Menu:
+                    menu.Update(mainMenu);
+                    break;
+                case GameState.Play:
+                    break;
+                case GameState.Exit:
+                    Exit();
+                    break;
+                case GameState.Info:
+                    menu.Update(infoMenu);
+                    break;
+                case GameState.Settings:
+                    menu.Update(settingsMenu);
+                    break;
+                case GameState.Extensions:
+                    menu.Update(extensionsMenu);
+                    break;
+                case GameState.FullScreen:
+                    //System.Drawing.Size res = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Size;
+                    //_graphics.PreferredBackBufferWidth = res.Width;
+                    //_graphics.PreferredBackBufferHeight = res.Height;
+                    //_graphics.IsFullScreen = true;
+                    Window.IsBorderless = true;
+                    Window.Position = new Point(0, 0);
+                    _graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+                    _graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+                    _graphics.ApplyChanges();
+                    gameState = GameState.Extensions;
+                    break;
+                case GameState.Window:
+                    Window.IsBorderless = false;
+                    _graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width / 2;
+                    _graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height / 2;
+                    _graphics.ApplyChanges();
+                    gameState = GameState.Extensions;
+                    break;
+                case GameState.WindowFrame:
+                    Window.IsBorderless = false;
+                    System.Drawing.Size res2 = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Size;
+                    _graphics.PreferredBackBufferWidth = res2.Width / 4;
+                    _graphics.PreferredBackBufferHeight = res2.Height / 4;
+                    _graphics.ApplyChanges();
+                    gameState = GameState.Extensions;
+                    break;
+                case GameState.Sound:
+                    menu.Update(soundMenu);
+                    break;
+                case GameState.GameMenu:
+                    menu.Update(gameMenu);
+                    break;
+                default:
+                    break;
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+
+            bg.Draw(_spriteBatch);
+
+            switch (gameState)
+            {
+                case GameState.Menu:
+                    menu.Draw(_spriteBatch, mainMenu, new Vector2(_graphics.PreferredBackBufferWidth / 2,
+                        _graphics.PreferredBackBufferHeight / 2));
+                    break;
+                case GameState.Play:
+                    break;
+                case GameState.Info:
+                    _spriteBatch.DrawString(spriteFont, info, new Vector2(_graphics.PreferredBackBufferWidth / 2 - info.Length * 3,
+                        _graphics.PreferredBackBufferHeight / 2), Color.MediumPurple);
+                    menu.Draw(_spriteBatch, infoMenu, new Vector2(_graphics.PreferredBackBufferWidth / 2,
+                        _graphics.PreferredBackBufferHeight / 2 + _graphics.PreferredBackBufferHeight / 5));
+                    break;
+                case GameState.Settings:
+                    menu.Draw(_spriteBatch, settingsMenu, new Vector2(_graphics.PreferredBackBufferWidth / 2,
+                        _graphics.PreferredBackBufferHeight / 2));
+                    break;
+                case GameState.Extensions:
+                    menu.Draw(_spriteBatch, extensionsMenu, new Vector2(_graphics.PreferredBackBufferWidth / 2,
+                        _graphics.PreferredBackBufferHeight / 2));
+                    break;
+                case GameState.FullScreen:
+                    Rectangle rec = new Rectangle(0, 0, GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width,
+                        GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height);
+                    bg.Rec = rec;
+                    bg.Draw(_spriteBatch);
+                    menu.Draw(_spriteBatch, extensionsMenu, new Vector2(_graphics.PreferredBackBufferWidth / 2,
+                        _graphics.PreferredBackBufferHeight / 2));
+                    break;
+                case GameState.Window:
+                    Rectangle rec1 = new Rectangle(0, 0, GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width / 2,
+                        GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height / 2);
+                    bg.Rec = rec1;
+                    bg.Draw(_spriteBatch);
+                    menu.Draw(_spriteBatch, extensionsMenu, new Vector2(_graphics.PreferredBackBufferWidth / 2,
+                        _graphics.PreferredBackBufferHeight / 2));
+                    break;
+                case GameState.WindowFrame:
+                    Rectangle rec2 = new Rectangle(0, 0, GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width / 4,
+                        GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height / 4);
+                    bg.Rec = rec2;
+                    bg.Draw(_spriteBatch);
+                    menu.Draw(_spriteBatch, extensionsMenu, new Vector2(_graphics.PreferredBackBufferWidth / 2,
+                        _graphics.PreferredBackBufferHeight / 2));
+                    break;
+                case GameState.Sound:
+                    menu.Draw(_spriteBatch, soundMenu, new Vector2(_graphics.PreferredBackBufferWidth / 2,
+                        _graphics.PreferredBackBufferHeight / 2));
+                    break;
+                case GameState.GameMenu:
+                    menu.Draw(_spriteBatch, gameMenu, new Vector2(_graphics.PreferredBackBufferWidth / 2,
+                        _graphics.PreferredBackBufferHeight / 2));
+                    break;
+                default:
+                    break;
+            }
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ menu/UI/Menu.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+using System.Media;
+using Microsoft.Xna.Framework.Media;
+using WMPLib;
+
+namespace menu.UI
+{
+    class Menu
+    {
+        private int selected = 0;
+        private KeyboardState keyboard;
+        private KeyboardState prevKeyboard;
+        private Vector2 size = new Vector2(0, 0);
+
+        private static int msize = 10;
+        private static int ssize = 10;
+        private Bar musicBar = new Bar(msize);
+        private Bar soundBar = new Bar(10);
+        private Bar backBar = new Bar(ssize);
+
+        //private Song selectSound;
+        private WindowsMediaPlayer WMP = new WindowsMediaPlayer();
+        private WindowsMediaPlayer enterWMP = new WindowsMediaPlayer();
+
+        private SpriteFont spriteFont;
+
+        public Vector2 Size
+        {
+            get { return size; }
+        }
+
+        public void LoadContent(ContentManager Content)
+        {
+            spriteFont = Content.Load<SpriteFont>("MenuFont");
+            //selectSound = Content.Load<Song>("selectsound");
+            WMP.URL = @"C:\Users\User\source\repos\menu\menu\Content\selectsound.mp3";
+            enterWMP.URL = @"C:\Users\User\source\repos\menu\menu\Content\entersound.mp3";
+            backBar.LoadContent(Content, "lowbar");
+            musicBar.LoadContent(Content, "highbar");
+            soundBar.LoadContent(Content, "highbar");
+        }
+
+        public void Draw(SpriteBatch brush, List<string> glist, Vector2 pos)
+        {
+
+            Color color;
+            for (int i = 0; i < glist.Count; i++)
+            {
+                if (selected == i)
+                {
+                    color = Color.Aqua;
+                }
+                else
+                {
+                    color = Color.HotPink;
+                }
+                brush.DrawString(spriteFont, glist[i], new Vector2(pos.X - 12 * glist.Count + 20 * i, pos.Y - 12 * glist.Count + 20 * i), color);
+            }
+            if (Game1.gameState == GameState.Sound)
+            {
+                musicBar.Draw(brush, new Vector2(pos.X - 12 * glist.Count + 50, pos.Y - 12 * glist.Count - 3));
+                backBar.Draw(brush, new Vector2(pos.X - 12 * glist.Count + 50, pos.Y - 12 * glist.Count - 3));
+                soundBar.Draw(brush, new Vector2(pos.X - 12 * glist.Count + 65, pos.Y - 12 * glist.Count + 18));
+                backBar.Draw(brush, new Vector2(pos.X - 12 * glist.Count + 65, pos.Y - 12 * glist.Count + 18));
+            }
+        }
+
+        public void Update(List<string> glist)
+        {
+            keyboard = Keyboard.GetState();
+            if (keyboard.IsKeyDown(Keys.S) && (keyboard.IsKeyDown(Keys.S) != prevKeyboard.IsKeyDown(Keys.S)))
+            {
+                if (selected < glist.Count - 1)
+                {
+                    selected++;
+                    WMP.controls.play();
+                    //MediaPlayer.Play(selectSound);
+                }
+
+            }
+            if (keyboard.IsKeyDown(Keys.W) && (keyboard.IsKeyDown(Keys.W) != prevKeyboard.IsKeyDown(Keys.W)))
+            {
+                if (selected > 0)
+                {
+                    selected--;
+                    WMP.controls.play();
+                    //MediaPlayer.Play(selectSound);
+                }
+
+            }
+
+            if (Game1.gameState == GameState.Menu && prevKeyboard != keyboard)
+            {
+                if (keyboard.IsKeyDown(Keys.Enter))
+                {
+                    enterWMP.controls.play();
+                    switch (selected)
+                    {
+                        case 0:
+                            Game1.gameState = GameState.Play;
+                            break;
+                        case 1:
+                            Game1.gameState = GameState.Play;
+                            break;
+                        case 2:
+                            Game1.gameState = GameState.Settings;
+                            selected = 0;
+                            break;
+                        case 3:
+                            Game1.gameState = GameState.Info;
+                            selected = 0;
+                            break;
+                        case 4:
+                            Game1.gameState = GameState.Exit;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                prevKeyboard = keyboard;
+            }
+            else if (Game1.gameState == GameState.Settings && prevKeyboard != keyboard)
+            {
+                if (keyboard.IsKeyDown(Keys.Enter))
+                {
+                    enterWMP.controls.play();
+                    switch (selected)
+                    {
+                        case 0:
+                            Game1.gameState = GameState.Extensions;
+                            selected = 0;
+                            break;
+                        case 1:
+                            Game1.gameState = GameState.Sound;
+                            selected = 0;
+                            break;
+                        case 2:
+                            if (Game1.prevGameState == GameState.Menu)
+                            {
+                                Game1.gameState = GameState.Menu;
+                                selected = 0;
+                            }
+                            else if (Game1.prevGameState == GameState.GameMenu)
+                            {
+                                Game1.gameState = GameState.GameMenu;
+                                selected = 0;
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                prevKeyboard = keyboard;
+            }
+            else if (Game1.gameState == GameState.Extensions && prevKeyboard != keyboard)
+            {
+                if (keyboard.IsKeyDown(Keys.Enter))
+                {
+                    enterWMP.controls.play();
+                    switch (selected)
+                    {
+                        case 0:
+                            Game1.gameState = GameState.FullScreen;
+                            break;
+                        case 1:
+                            Game1.gameState = GameState.Window;
+                            break;
+                        case 2:
+                            Game1.gameState = GameState.WindowFrame;
+                            break;
+                        case 3:
+                            Game1.gameState = GameState.Settings;
+                            selected = 0;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                prevKeyboard = keyboard;
+            }
+            else if (Game1.gameState == GameState.Info && prevKeyboard != keyboard)
+            {
+                if (keyboard.IsKeyDown(Keys.Enter))
+                {
+                    enterWMP.controls.play();
+                    switch (selected)
+                    {
+                        case 0:
+                            Game1.gameState = GameState.Menu;
+                            selected = 0;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                prevKeyboard = keyboard;
+            }
+            else if (Game1.gameState == GameState.Sound && prevKeyboard != keyboard)
+            {
+                switch (selected)
+                {
+                    case 0:
+                        if (keyboard.IsKeyDown(Keys.Left) && msize > 0)
+                        {
+                            musicBar.Update(msize -= 1);
+                        }
+                        if (keyboard.IsKeyDown(Keys.Right) && msize < 10)
+                        {
+                            musicBar.Update(msize += 1);
+                        }
+                        break;
+                    case 1:
+                        if (keyboard.IsKeyDown(Keys.Left) && ssize > 0)
+                        {
+                            soundBar.Update(ssize -= 1);
+                            WMP.settings.volume = ssize * 10;
+                            enterWMP.settings.volume = ssize * 10;
+                        }
+                        if (keyboard.IsKeyDown(Keys.Right) && ssize < 10)
+                        {
+                            soundBar.Update(ssize += 1);
+                            WMP.settings.volume = ssize * 10;
+                            enterWMP.settings.volume = ssize * 10;
+                        }
+                        break;
+                    case 2:
+                        if (keyboard.IsKeyDown(Keys.Enter))
+                        {
+                            enterWMP.controls.play();
+                            Game1.gameState = GameState.Settings;
+                            selected = 0;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                prevKeyboard = keyboard;
+            }
+            else if (Game1.gameState == GameState.GameMenu && prevKeyboard != keyboard)
+            {
+                if (keyboard.IsKeyDown(Keys.Enter))
+                {
+                    enterWMP.controls.play();
+                    switch (selected)
+                    {
+                        case 0:
+
+                            break;
+                        case 1:
+
+                            break;
+                        case 2:
+                            Game1.gameState = GameState.Settings;
+                            selected = 0;
+                            break;
+                        case 3:
+                            Game1.gameState = GameState.Menu;
+                            selected = 0;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                prevKeyboard = keyboard;
+            }
+        }
+
+        /*public void SetMenuPosition(Vector2 position,List<string> glist)
+
+        {
+            Position = position;
+            for (int i = 0; i < glist.Count; i++)
+            {
+                glist[i].Position = position;
+                position.Y += 30;
+            }
+        }
+        */
+        /*private void SetSizeWidth()
+        {
+            string biggestWord = "";
+            int max = 0;
+            foreach (var txt in )
+            {
+                if (txt.Length > max)
+                {
+                    biggestWord = txt;
+                    max = txt.Length;
+                }
+            }
+            Console.WriteLine(biggestWord);
+            size.X = biggestWord.Length * 8;
+        }
+        */
+    }
+}
